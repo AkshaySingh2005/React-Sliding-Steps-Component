033b91ab4f498670aebe549f4704e05273829625
AkshaySingh2005 <156520821+AkshaySingh2005@users.noreply.github.com>
2024-09-02 22:46:29 +0530

    Replace all code with new version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your  income 🤑",
+];
+
+export default function App() {
+  //^ STATE
+  const [step, setStep] = useState(1); //^ Hooks
+  const [isOpen, setIsopen] = useState(true);
+  const [name, setName] = useState({ name: "akshay singh" });
+
+  function handlePrevious() {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      setStep(step + 1);
+      setName({ name: "akshay" });
+    }
+  }
+
+  if (isOpen) {
+    return (
+      <div className="steps">
+        <button className="close" onClick={() => setIsopen(false)}>X</button>
+
+        <div className="numbers">
+          <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+          <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+          <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        </div>
+
+        <p className="message">
+          Step {step}: {messages[step - 1]} {name.name}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+        <div className="buttons">
+          <button
+            style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            onClick={handlePrevious}
+          >
+            Previous
+          </button>
+          <button
+            style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            onClick={handleNext}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    );
+  } else {
+    return <h1>Oops!</h1>;
+  }
+}
